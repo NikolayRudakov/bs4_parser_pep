7fe63bb121aa68eeb2883afa0ceaa218661c44d4
NikolayRudakov <nrudakov@yandex.ru>
2022-12-20 23:07:53 +0300

    after review commit 20.12

--- src/configs.py
+++ src/configs.py
@@ -4,57 +4,36 @@ import logging
 
 from logging.handlers import RotatingFileHandler
 
-from constants import BASE_DIR
+from constants import BASE_DIR, OUTPUT_CHOICE_ARGUMENTS
 
 # Описание формата логов:
-# Время записи – Уровень сообщения – Cообщение.
+# # Время записи – Уровень сообщения – Cообщение.
 LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
-# Указываем формат времени.
-DT_FORMAT = '%d.%m.%Y %H:%M:%S'
+DT_FORMAT = "%d.%m.%Y %H:%M:%S"
 
 
 def configure_argument_parser(available_modes):
-    parser = argparse.ArgumentParser(description='Парсер документации Python')
+    parser = argparse.ArgumentParser(description="Парсер документации Python")
+    parser.add_argument("mode", choices=available_modes, help="Режимы работы парсера")
+    parser.add_argument("-c", "--clear-cache", action="store_true", help="Очистка кеша")
     parser.add_argument(
-        'mode',
-        choices=available_modes,
-        help='Режимы работы парсера'
-    )
-    parser.add_argument(
-        '-c',
-        '--clear-cache',
-        action='store_true',
-        help='Очистка кеша'
-    )
-    parser.add_argument(
-        '-o',
-        '--output',
-        choices=('pretty', 'file'),
-        help='Дополнительные способы вывода данных'
+        "-o",
+        "--output",
+        choices=OUTPUT_CHOICE_ARGUMENTS,
+        help="Дополнительные способы вывода данных",
     )
     return parser
 
 
 def configure_logging():
-    # Сформируйте путь до директории logs.
-    log_dir = BASE_DIR / 'logs'
-    # Создайте директорию.
+    log_dir = BASE_DIR / "logs"
     log_dir.mkdir(exist_ok=True)
-    # Получение абсолютного пути до файла с логами.
-    log_file = log_dir / 'parser.log'
+    log_file = log_dir / "parser.log"
 
-    # Инициализация хендлера с ротацией логов.
-    # Максимальный объём одного файла — десять в шестой степени байт (10**6),
-    # максимальное количество файлов с логами — 5.
-    rotating_handler = RotatingFileHandler(
-        log_file, maxBytes=10 ** 6, backupCount=5
-    )
-    # Базовая настройка логирования basicConfig.
+    rotating_handler = RotatingFileHandler(log_file, maxBytes=10**6, backupCount=5)
     logging.basicConfig(
         datefmt=DT_FORMAT,
         format=LOG_FORMAT,
-        # Уровень записи логов.
         level=logging.INFO,
-        # Вывод логов в терминал.
-        handlers=(rotating_handler, logging.StreamHandler())
+        handlers=(rotating_handler, logging.StreamHandler()),
     )
--- src/constants.py
+++ src/constants.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 
-MAIN_DOC_URL = 'https://docs.python.org/3/'
-PEP_URL = 'https://peps.python.org/'
+MAIN_DOC_URL = "https://docs.python.org/3/"
+PEP_URL = "https://peps.python.org/"
 BASE_DIR = Path(__file__).parent
-DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+DATETIME_FORMAT = "%Y-%m-%d_%H-%M-%S"
+OUTPUT_CHOICE_ARGUMENTS = ("pretty", "file")
 EXPECTED_STATUS = {
-    'A': ('Active', 'Accepted'),
-    'D': ('Deferred',),
-    'F': ('Final',),
-    'P': ('Provisional',),
-    'R': ('Rejected',),
-    'S': ('Superseded',),
-    'W': ('Withdrawn',),
-    '': ('Draft', 'Active'),
+    "A": ("Active", "Accepted"),
+    "D": ("Deferred",),
+    "F": ("Final",),
+    "P": ("Provisional",),
+    "R": ("Rejected",),
+    "S": ("Superseded",),
+    "W": ("Withdrawn",),
+    "": ("Draft", "Active"),
 }
